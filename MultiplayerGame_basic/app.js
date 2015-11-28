f24d39f634a01cc908ea4c98e1426257a5ea7547
Fabi <fabi.images@gmail.com>
2015-11-28 18:08:27 +0100

    renamed some variables for more clarity

--- MultiplayerGame_basic/app.js
+++ MultiplayerGame_basic/app.js
@@ -9,7 +9,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var clients = {};
+var devices = {};
 
 // include external data, eg. css, images (public folder)
 app.use(express.static(__dirname + '/public'));
@@ -27,32 +27,32 @@ http.listen(port, function () {
 
 // Socket.io: Communication Server <-> Client(s)
 io.on('connection', function (socket) {
-    var client_ip = socket.request.connection.remoteAddress;
+    var device_ip = socket.request.connection.remoteAddress;
 
     /**
      * 5. If one client tries to connect with multiple browsers
      */
-    if ((contains(clients, client_ip))) {
-        clients[client_ip].amountOfClients++;
-        console.log("Amount of clients for device " + client_ip + ": " + clients[client_ip].amountOfClients);
+    if ((contains(devices, device_ip))) {
+        devices[device_ip].amountOfClients++;
+        console.log("Amount of clients for device " + device_ip + ": " + devices[device_ip].amountOfClients);
 
         var multiConnection = {
-            clients: clients,
-            ip_address: client_ip
+            devices: devices,
+            ip_address: device_ip
         }
         socket.emit("multiConnection", multiConnection);
     }
 
     // New client is getting connected
-    if (!(contains(clients, client_ip))) {
+    if (!(contains(devices, device_ip))) {
         var socket_data = {};
 
         /**
-         * 1. socket.emit sends the data of all connected clients only to the new one
+         * 1. socket.emit sends the data of all connected devices only to the new one
          */
 
-        console.log("Existing players: " + JSON.stringify(clients));
-        socket.emit("drawExistingPlayers", clients);
+        console.log("All connected devices: " + JSON.stringify(devices));
+        socket.emit("drawExistingPlayers", devices);
 
         /**
          * 1.1. Add new client
@@ -63,17 +63,17 @@ io.on('connection', function (socket) {
         socket_data.posY = Math.floor((Math.random() * 700) + 0);
         socket_data.amountOfClients = 1;
 
-        clients[client_ip] = socket_data;
-        console.log("New Player: " + JSON.stringify(clients[client_ip]));
-        console.log("All connected clients: " + JSON.stringify(clients));
+        devices[device_ip] = socket_data;
+        console.log("New Player: " + JSON.stringify(devices[device_ip]));
+        console.log("All connected devices: " + JSON.stringify(devices));
 
         /**
-         * 2. io.emit sends the data of the newly added client to all connected clients
+         * 2. io.emit sends the data of the newly added device to all connected clients
          */
 
         var newPlayer = {
-            key: client_ip,
-            val: clients[client_ip]
+            key: device_ip,
+            val: devices[device_ip]
         }
         io.emit("drawNewPlayer", newPlayer);
     }
@@ -85,8 +85,8 @@ io.on('connection', function (socket) {
 
     socket.on('newPosition', function (data) {
         console.log("Position changed: " + JSON.stringify(data));
-        clients[data.client_ip].posX = data.posX;
-        clients[data.client_ip].posY = data.posY;
+        devices[data.device_ip].posX = data.posX;
+        devices[data.device_ip].posY = data.posY;
         io.emit('newPosition', data);
     });
 
@@ -97,14 +97,14 @@ io.on('connection', function (socket) {
      */
 
     socket.on('disconnect', function () {
-        clients[client_ip].amountOfClients--;
-        console.log("Amount of clients for device " + client_ip + ": " + clients[client_ip].amountOfClients);
-        if (clients[client_ip].amountOfClients <= 0) {
-            io.emit('player_disconnect', client_ip);
-            console.log(client_ip + ' disconnected');
-            delete clients[client_ip];
+        devices[device_ip].amountOfClients--;
+        console.log("Amount of clients for device " + device_ip + ": " + devices[device_ip].amountOfClients);
+        if (devices[device_ip].amountOfClients <= 0) {
+            io.emit('player_disconnect', device_ip);
+            console.log(device_ip + ' disconnected');
+            delete devices[device_ip];
         }
-        console.log("All connected clients: " + JSON.stringify(clients));
+        console.log("All connected devices: " + JSON.stringify(devices));
     });
 });
 
